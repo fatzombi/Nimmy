4263805da0dba47db1dd9bec38cb10744d20f304
FATZOMBI <fatzombi@outlook.in>
2016-12-28 15:24:09 +0530

    remove long description

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 setup(name='Nimmy',
       version='0.1',
-      long_description=read('README.md'),
       description='a telegram bot api client/pattern built on top of telepot',
       url='https://github.com/fatzombi/Nimmy',
       keywords='telepot telegram api client extension',
